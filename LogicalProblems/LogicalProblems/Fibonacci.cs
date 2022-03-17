78ac8681fe0efdebc33b4803eabf9c1bd3ebda8d
Remant <remantmahato9798@gmail.com>
2022-03-17 05:37:51 +0530

    Added : UC1-FibbonaciSeries

---
+++ LogicalProblems/LogicalProblems/Fibonacci.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalProblems
+
+
+{
+    internal class Fibbonacci
+    {
+        public void FibbonacciSeries()
+        {
+            Console.WriteLine("Enter the Number");
+            int Number = int.Parse(Console.ReadLine());
+
+            int Num_a = 0;
+            int Num_b = 1;
+            int Sum;
+            Console.Write(Num_a + " " + Num_b +" ");
+            for (int i = 2; i < Number; i++) 
+            {
+                Sum = Num_a + Num_b;
+                Console.Write(Sum + " ");
+                Num_a = Num_b;
+                Num_b = Sum;
+            }
+        }
+    }
+}
--- LogicalProblems/LogicalProblems/Program.cs
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -7,8 +7,18 @@ namespace LogicalProblems
         public static void Main(String[] args)
         {
             Console.WriteLine("Enter your Option");
-            
-           
+            Console.WriteLine("Select 1: Fibonacci Series");
+
+            int Option = int.Parse(Console.ReadLine());
+            switch (Option)
+            {
+                case 1:
+                    Fibbonacci fibonaci = new Fibbonacci();
+                    fibonaci.FibbonacciSeries();
+                    break;
+
+            }
+
 
         }
     }
