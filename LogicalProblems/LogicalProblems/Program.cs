65cbeb42e72471f32890eef962e008384fc5ba22
Remant <remantmahato9798@gmail.com>
2022-03-17 05:45:01 +0530

    Added : UC3-PrimeNumber

--- LogicalProblems/LogicalProblems/Program.cs
+++ LogicalProblems/LogicalProblems/Program.cs
@@ -9,6 +9,7 @@ namespace LogicalProblems
             Console.WriteLine("Enter your Option");
             Console.WriteLine("Select 1: Fibonacci Series");
             Console.WriteLine("Select 2: Perfect NUmber");
+            Console.WriteLine("Select 3: Prime Number");
 
             int Option = int.Parse(Console.ReadLine());
             switch (Option)
@@ -21,7 +22,10 @@ namespace LogicalProblems
                     PerfectNum Perfect = new PerfectNum();
                     Perfect.PerfectNumber();
                     break;
-
+                case 3:
+                    PrimeNum Prime = new PrimeNum();
+                    Prime.PrimeNumber();
+                    break;
             }
 
 
